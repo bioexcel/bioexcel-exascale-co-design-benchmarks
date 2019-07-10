11d06992141ccd36e1545be6a870b85ca5e7add0
Mark Abraham <mark.j.abraham@gmail.com>
2019-07-10 18:11:57 +0200

    Update script that makes release tarball

--- make-tarballs.sh
+++ make-tarballs.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # This script is a convenience for making BioExcel exascale co-design
-# benchmark releases
+# benchmark releases. Run it from the top level directory of the git
+# repository.
 #
 # The resulting tarball contains code and inputs relevant for all
 # BioExcel core applications. Details are found in the respective
@@ -11,18 +12,25 @@
 # so we can put that on the BioExcel webpage easily.
 
 version=0.1.0
-
-tar cfz BioExcel-co-design-benchmarks-$version.tar.gz GROMACS HADDOCK CP2K
+tarball_prefix=BioExcel-co-design-benchmarks
+tarball_name=$tarball_prefix-$version.tgz
+dir_name=bioexcel-exascale-co-design-benchmarks
+(
+    rm -f $tarball_prefix*.tgz
+    cd ..
+    tar cfz /tmp/$tarball_name --exclude=.git* $dir_name
+    mv /tmp/$tarball_name $dir_name
+)
 
 echo "sha1sum values"
-for tarballs in *.tar.gz
+for tarballs in *.tgz
 do
     sha1sum $tarballs
 done
 
 echo
 echo "sha256sum values"
-for tarballs in *.tar.gz
+for tarballs in *.tgz
 do
     sha256sum $tarballs
 done
